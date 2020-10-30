94ed0091fc57ee6d2e8a915a548b1fbfc0a3ea06
qrtz <qrtz@mail.ru>
2020-10-30 12:55:52 +0300

    Initial commit

---
+++ src/main/java/com/bell/bellpractive/BellpractiveApplication.java
@@ -0,0 +1,13 @@
+package com.bell.bellpractive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellpractiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BellpractiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bell/bellpractive/BellpractiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bell.bellpractive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellpractiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
